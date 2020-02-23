e4e1c4bfe1a4154b05a6ce3635dc29ebc949baf5
Hector Becerril Medina <hector.becerril@biaani.com>
2020-02-22 23:14:22 -0600

    Initial Commit - Load base project

---
+++ onlinebookstore/src/test/java/com/biaani/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biaani.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
