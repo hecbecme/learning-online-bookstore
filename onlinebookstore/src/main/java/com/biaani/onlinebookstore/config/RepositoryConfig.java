adf8c5ef51d15a5aeb235cdeae8d01afaa5fd9eb
Hector Becerril Medina <hector.becerril@biaani.com>
2020-02-23 01:30:34 -0600

    Completed the backend for feature one.

---
+++ onlinebookstore/src/main/java/com/biaani/onlinebookstore/config/RepositoryConfig.java
@@ -0,0 +1,19 @@
+package com.biaani.onlinebookstore.config;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+	
+	private EntityManager entityManager;
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(Type::getJavaType).toArray(Class[]::new)); 
+	}
+}
